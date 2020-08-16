7eaefc975d6289b241301336d6e1e0c0f7d7cbde
Nikairu <diogosilvaoliveirawork@gmail.com>
2020-08-16 12:20:02 +0100

    Added pokemon array with 3 pokemon to scripts file, created empty css and html files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var pokemonList = [];
+
+pokemonList[0] = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+};
+pokemonList[1] = {
+    name: 'Blastoise',
+    height: 1.6,
+    types: ['water']
+};
+pokemonList[2] = {
+    name: 'Blaziken',
+    height: 1.9,
+    types: ['fire', 'fighting']
+};
+
+console.log(pokemonList);
