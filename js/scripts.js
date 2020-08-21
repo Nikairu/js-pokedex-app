ed98aac23f1ce9c119ab0043e6ac855a69be11cf
Nikairu <diogosilvaoliveirawork@gmail.com>
2020-08-21 21:01:55 +0100

    added IIFE, created getAll, add, filterByName, FilterByMinimumHeight functions, for each now loops through the return of getAll function, add function checks if what's being added is an object and has the same keys as other pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -20,37 +20,68 @@ var pokemonRepository = (function () {
 
     ];
 
-    function getAll(){
+    function getAll() {
         return pokemonList;
     }
-    function add(item){
-        console.log(typeof(item));
-        pokemonList.push(item);
+
+    function add(item) {
+
+        if (typeof (item) === 'object' && item.hasOwnProperty('name') && item.hasOwnProperty('height') && item.hasOwnProperty('types')) {
+            pokemonList.push(item);
+            console.log(`${item.name} added to the repository`);
+        } else {
+            console.log(`this is not a pokemon`);
+        }
+
     }
 
-    return{
+    function filterByName(searchValue){
+
+        return pokemonList.filter(item => item.name===searchValue);
+    }
+
+    function filterByMinimumHeight(searchValue){
+
+        return pokemonList.filter(item => item.height>searchValue);
+    }
+
+
+    return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        filterByName: filterByName,
+        filterByMinimumHeight: filterByMinimumHeight
     }
 
 
 })();
 
+pokemonRepository.add({
+    name: 'Cubone',
+    height: 0.4,
+    types: ['ground']
+
+});
+
+
 
 pokemonRepository.getAll().forEach(pokemon => {
-    if(pokemon.height<1){  
+    if (pokemon.height < 1) {
         document.write(`${pokemon.name} (height: ${pokemon.height})<br>`)
     }
-    else{
+    else {
         document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow that's big!<br>`)
     }
 });
 
-pokemonRepository.add({
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['grass', 'poison']
 
+let searchResult = pokemonRepository.filterByName('Blaziken');
+searchResult.forEach(pokemon => {
+    document.write(`Found ${pokemon.name} by name and this is his types: ${pokemon.types.join(", ")}<br>`);
 });
 
-//console.log(pokemonList);
+
+searchResult = pokemonRepository.filterByMinimumHeight(0.6);
+searchResult.forEach(pokemon => {
+    document.write(`Found ${pokemon.name} by minumum height and this is his height: ${pokemon.height}<br>`);
+});
