d654bbcae17c26867709e9eb62d5957c4f70e53f
Nikairu <diogosilvaoliveirawork@gmail.com>
2020-09-19 14:25:31 +0100

    Added styles to button, butons are now created dynamically based on how many pokemon objects we have with new function in IIFE addListItem

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,23 @@ var pokemonRepository = (function () {
     }
 
 
+    function addListItem(pokemon) {
+
+        let pokemonBlock = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon-button');
+    
+        listItem.appendChild(button);
+        pokemonBlock.appendChild(listItem);
+
+    }
+
+
+
     return {
+        addListItem: addListItem,
         add: add,
         getAll: getAll,
         filterByName: filterByName,
@@ -66,22 +82,20 @@ pokemonRepository.add({
 
 
 pokemonRepository.getAll().forEach(pokemon => {
-    if (pokemon.height < 1) {
-        document.write(`${pokemon.name} (height: ${pokemon.height})<br>`)
-    }
-    else {
-        document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow that's big!<br>`)
-    }
+
+    pokemonRepository.addListItem(pokemon);
+    
 });
 
 
 let searchResult = pokemonRepository.filterByName('Blaziken');
 searchResult.forEach(pokemon => {
-    document.write(`Found ${pokemon.name} by name and this is his types: ${pokemon.types.join(", ")}<br>`);
+
+    //document.write(`Found ${pokemon.name} by name and this is his types: ${pokemon.types.join(", ")}<br>`);
 });
 
 
 searchResult = pokemonRepository.filterByMinimumHeight(0.6);
 searchResult.forEach(pokemon => {
-    document.write(`Found ${pokemon.name} by minumum height and this is his height: ${pokemon.height}<br>`);
+    //document.write(`Found ${pokemon.name} by minumum height and this is his height: ${pokemon.height}<br>`);
 });
