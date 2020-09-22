9061cc5e6354f29d3dd98520f87288cbf0c7d4dd
Nikairu <diogosilvaoliveirawork@gmail.com>
2020-09-22 14:06:32 +0100

    Created modal and functions to display it with pokemon info and hide it

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,7 @@ let pokemonRepository = (function () {
 	
 	function showLoadingMessage(){
 		let pokemonImage = document.querySelector('.pokemon-sprite');
-		pokemonImage.style.display = "none";//hides previous pokemon sprite
+		//pokemonImage.style.display = "none";//hides previous pokemon sprite
         //pokemonImage.src = ``;
 		
 		let infoContainerObj =  document.querySelector('.pokemon-info-container');
@@ -117,12 +117,78 @@ let pokemonRepository = (function () {
         pokemonRepository.loadDetails(pokemon).then(function () {
 			hideLoadingMessage();
 
-			let pokemonImage = document.querySelector('.pokemon-sprite');
-            pokemonImage.src = `${pokemon.imageUrl}`;//displays new pokemon sprite
-			pokemonImage.style.display = "block";
+			showModal(pokemon);
+
+			//let pokemonImage = document.querySelector('.pokemon-sprite');
+            //pokemonImage.src = `${pokemon.imageUrl}`;//displays new pokemon sprite
+			//pokemonImage.style.display = "block";
 			console.log(`<Mandatory Console Log> Pokemon Height: ${pokemon.height}`)
 		})
     }
+
+	let modalContainer = document.querySelector('.modal-container');
+ 	function showModal(pokemon) {
+		modalContainer.innerHTML = '';
+		let modal = document.createElement('div');
+		modal.classList.add('modal');
+
+		let closeButtonElement = document.createElement('button');
+		closeButtonElement.classList.add('modal-close');
+		closeButtonElement.innerText = 'Close';
+		closeButtonElement.addEventListener('click', hideModal);
+
+		let titleElement = document.createElement('h1');
+		titleElement.classList.add(`modal-title`);
+		titleElement.innerText = pokemon.name;
+
+		let contentElement = document.createElement('p');
+		contentElement.classList.add(`modal-content`);
+		contentElement.innerText = `Height: ${pokemon.height}`;
+
+		let imageContainer = document.createElement('div');
+		imageContainer.classList.add(`modal-image-container`);
+
+		let imageElement = document.createElement('img');
+		imageElement.classList.add(`modal-image`);
+		imageElement.src = `${pokemon.imageUrl}`;
+
+		modal.appendChild(closeButtonElement);
+		modal.appendChild(titleElement);
+		modal.appendChild(contentElement);
+		imageContainer.appendChild(imageElement);
+		modal.appendChild(imageContainer);
+		modalContainer.appendChild(modal);
+
+
+		modalContainer.classList.add('is-visible');
+ 	}
+
+  	let dialogPromiseReject; // This can be set later, by showDialog
+
+  	function hideModal() {
+    	let modalContainer = document.querySelector('.modal-container');
+    	modalContainer.classList.remove('is-visible');
+
+    	if (dialogPromiseReject) {
+     	 	dialogPromiseReject();
+      	dialogPromiseReject = null;
+    	}
+	 }
+	 
+	window.addEventListener('keydown', (e) => {
+		if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+		  hideModal();  
+		}
+	  });
+
+	modalContainer.addEventListener('click', (e) => {
+		// Since this is also triggered when clicking INSIDE the modal
+		// We only want to close if the user clicks directly on the overlay
+		let target = e.target;
+		if (target === modalContainer) {
+		  hideModal();
+		}
+	});
 
 
 
@@ -166,41 +232,3 @@ searchResult.forEach(pokemon => {
 searchResult.forEach(pokemon => {
     //document.write(`Found ${pokemon.name} by minumum height and this is his height: ${pokemon.height}<br>`);
 }); */
-
-
-
-
-
-
-
-
-function loadList() {
-	showLoadingMessage();
-
-	return fetch(apiUrl).then(function (promiseResponse){
-		return promiseResponse.json();
-	}).then(function (json){
-		json.results.forEach(function (item) {
-
-			let pokemon = {
-				name: item.name.charAt(0).toUpperCase() + item.name.slice(1),
-				detailsUrl: item.url,
-				types: '',
-				height: ''
-			};
-
-			fetch(item.url).then(function (response){
-				return response.json();
-			}).then(function (json){
-				pokemon.imageUrl = json.sprites.front_default;
-				pokemon.height = json.height;
-			});
-			add(pokemon);
-
-			})
-		hideLoadingMessage();
-	}).catch(function (){
-	   //Error 
-	   hideLoadingMessage();
-	})
-}
